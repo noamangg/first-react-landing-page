e3fe055bf7b65c472e57f133ced48ba8dc158fe5
Noaman <noamangg@gmail.com>
2023-09-08 23:53:38 +0300

    change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/first-react-landing-page',
+  base: '/first-react-landing-page/',
   plugins: [react()],
 })
